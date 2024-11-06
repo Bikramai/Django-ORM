fb0cd8656ff733609418680ec613ac27475cf280
bikramia <bikram.rhce@gmail.com>
2024-11-06 14:18:46 -0500

    Complex Lookups using Q Objects -2nd option chain the call to filter method

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -7,7 +7,7 @@ from store.models import Product
 
 def say_hello(request):
     # Products: inventory < 10 AND Price < 20
-    queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
     
         
     return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
