92cd43edda454b9a0e96fb37a281eaaf4377a5ed
bikramia <bikram.rhce@gmail.com>
2024-11-06 13:00:43 -0500

    Retrieving Objects :Better way to implement -None -by using .objects.filter(pk=0).first() and .exists()

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -6,9 +6,8 @@ from store.models import Product
 
 
 def say_hello(request):
-    try:
-        product = Product.objects.get(pk=0)
-    except ObjectDoesNotExist:
-        pass
+    # None
+    product = Product.objects.filter(pk=0).first()
+    
         
     return render(request, 'hello.html', {'name': 'Bikram'})
