bc51a7acff9fa3ff2fd9e28a431e2891b96a32b3
bikramia <bikram.rhce@gmail.com>
2024-11-06 16:26:47 -0500

    -get product by collection1 and sort by unit priceSorting

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -8,7 +8,7 @@ from store.models import Product
 
 def say_hello(request):
     # Products: inventory = price
-    queryset = Product.objects.order_by('unit_price', '-title').reverse()
+    queryset = Product.objects.filter(collection__id=1).order_by('unit_price')
     
         
     return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
