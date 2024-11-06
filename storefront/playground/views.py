ec0bd6c27a979cdd7dc6bccafb07117670fa68a3
bikramia <bikram.rhce@gmail.com>
2024-11-06 14:27:08 -0500

    How can we combine the condition using OR operator -by using Q Objects

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
@@ -6,8 +7,8 @@ from store.models import Product
 
 
 def say_hello(request):
-    # Products: inventory < 10 AND Price < 20
-    queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    # Products: inventory < 10 OR Price < 20
+    queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
     
         
     return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
