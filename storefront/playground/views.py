38d6a5c368ed73b74dd3d3675ed87d63fd602ac5
bikramia <bikram.rhce@gmail.com>
2024-11-06 17:09:16 -0500

    Limiting Reaults- product on the second page, start from 5 and finish before 10

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -8,7 +8,8 @@ from store.models import Product
 
 def say_hello(request):
     # 0, 1, 2, 3, 4
-    queryset = Product.objects.all()[:5]
+    # 5, 6, 7, 8, 9
+    queryset = Product.objects.all()[5:10]
     
         
     return render(request, 'hello.html', {'name': 'Bikram', 'products': queryset})
