3ca7045ceafe62ecdc5ff3ffa6a41a42b92bc3cc
bikramia <bikram.rhce@gmail.com>
2024-11-06 16:10:15 -0500

    Sorting -by reverse method

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -8,7 +8,7 @@ from store.models import Product
 
 def say_hello(request):
     # Products: inventory = price
-    queryset = Product.objects.order_by('-title')
+    queryset = Product.objects.order_by('unit_price', '-title').reverse()
     
         
     return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
