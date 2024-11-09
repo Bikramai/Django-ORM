43e6bca9f5611dce3683523808d3c342110c63bc
bikramia <bikram.rhce@gmail.com>
2024-11-09 12:19:44 -0500

    Get the last 5 orders with their customer and items(incliduging product)

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from django.db.models import Q, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from store.models import Product, Order
 
 
 
 def say_hello(request):
     # Select_related (1)
     # prefetch_related (n)
-    queryset = Product.objects.prefetch_related(
-        'promotions').select_related('collection').all()
+    queryset = Order.objects.select_related('customer').order_by('-placed_at')[:5]
     
        
-    return render(request, 'hello.html', {'name': 'Bikram', 'products': queryset})
+    return render(request, 'hello.html', {'name': 'Bikram', 'orders': queryset})
