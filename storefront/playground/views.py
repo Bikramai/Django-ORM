05213e2663f4bc1d2a6f3fe34a1771e7853bfbf1
bikramia <bikram.rhce@gmail.com>
2024-11-10 18:32:19 -0500

    Creating Objects or Inserting Objects

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from django.db.models import Value, F, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Product, Collection
 from tags.models import TaggedItem
 
 
 
 def say_hello(request):
-    # QuerySet Cache
-    queryset = Product.objects.all()
-    queryset[0]
-    list(queryset)
+    # Creating Objects or Inserting Objects 
+    # first approach
+    collection = Collection()
+    collection.title = 'Video Games'
+    collection.featured_product = Product(pk=1)
+    collection.save()
+    
+
+    # Second approach
+    # collection = Collection.objects.create(name='a', featured_product_id=1)
+    # collection.id
     
        
     return render(request, 'hello.html', {'name': 'Bikram'})
