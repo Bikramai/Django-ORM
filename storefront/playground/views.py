daee9c40108ffd7222badc742a59a3eef6f8e904
bikramia <bikram.rhce@gmail.com>
2024-11-10 13:42:30 -0500

    Calling Database Functions-short hand to achieve the CONCAT first and last name

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 from store.models import Product, Customer
 
 
@@ -11,5 +12,9 @@ def say_hello(request):
         full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
     )
     
+    queryset = Customer.objects.annotate(
+        # CONCAT
+        full_name=Concat('first_name', Value(' '), 'last_name')
+    )
        
     return render(request, 'hello.html', {'name': 'Bikram', 'result': list(queryset)})
