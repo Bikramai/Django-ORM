fe6bb4d681ade7ea07b90e716c48f82e24f46d1e
bikramia <bikram.rhce@gmail.com>
2024-11-10 15:13:32 -0500

    Working with Express Wrappers

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count
+from django.db.models import Value, F, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from store.models import Product, Customer
 
 
 
 def say_hello(request):
-    # Grouping Data
-    queryset = Customer.objects.annotate(
-        orders_count=Count('order')
+    # Expression Wrappers
+    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
+    
+    queryset = Product.objects.annotate(
+        discounted_price=discounted_price
     )
        
     return render(request, 'hello.html', {'name': 'Bikram', 'result': list(queryset)})
