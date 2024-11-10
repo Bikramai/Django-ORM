e42d24ddc84727df9f16b133e3775980eb659209
bikramia <bikram.rhce@gmail.com>
2024-11-10 17:00:48 -0500

    Custom Managers

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -8,15 +8,8 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    # Querying Generic Relationships
-    content_type = ContentType.objects.get_for_model(Product)
-
-    queryset = TaggedItem.objects \
-        .select_related('tag') \
-        .filter(
-            content_type=content_type, 
-            object_id=1
-        )
+    # Custom Managers
+    TaggedItem.objects.get_tags_for(Product, 1) 
     
        
-    return render(request, 'hello.html', {'name': 'Bikram', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Bikram'})
