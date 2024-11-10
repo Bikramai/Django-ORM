038875b531174c53e53880800096287689d74d72
bikramia <bikram.rhce@gmail.com>
2024-11-10 14:30:11 -0500

    Grouping Data -Count(order)

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func
+from django.db.models import Value, F, Func, Count
 from django.db.models.functions import Concat
 from store.models import Product, Customer
 
 
 
 def say_hello(request):
-    # Annotate Objects
+    # Grouping Data
     queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
-    )
-    
-    queryset = Customer.objects.annotate(
-        # CONCAT
-        full_name=Concat('first_name', Value(' '), 'last_name')
+        orders_count=Count('order')
     )
        
     return render(request, 'hello.html', {'name': 'Bikram', 'result': list(queryset)})
