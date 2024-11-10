d232ed9572060e7aadb3e3ee60a89a656c55d707
bikramia <bikram.rhce@gmail.com>
2024-11-10 12:25:28 -0500

    Annotate Objects -Value(True)

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.db.models import Q, F
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db.models import Value, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, Order
+from store.models import Product, Customer
 
 
 
 def say_hello(request):
-    # Aggregating Objects
-    result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
+    # Annotate Objects
+    queryset = Customer.objects.annotate(is_new=Value(True))
     
        
-    return render(request, 'hello.html', {'name': 'Bikram', 'result': result})
+    return render(request, 'hello.html', {'name': 'Bikram', 'result': list(queryset)})
