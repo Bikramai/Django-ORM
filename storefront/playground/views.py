2fff86e4380653c4baadf0493c481e28e7848ae5
bikramia <bikram.rhce@gmail.com>
2024-11-11 17:40:56 -0500

    Transactions

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,17 +2,26 @@ from django.shortcuts import render
 from django.db.models import Value, F, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Collection
+from django.db import transaction
+from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
 
 
-
 def say_hello(request):
-    # Deleting Objects 
-    collection = Collection(pk=11)
-    collection.delete()
-    
-    Collection.objects.filter(id__gt=5).delete()
 
+    # ...
+
+    # Transactions
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, 'hello.html', {'name': 'Bikram'})
