6bb4239c33184c25a8b809304a0931d8b249d20e
bikramia <bikram.rhce@gmail.com>
2024-11-11 18:30:53 -0500

    Executing Raw SQL Queries

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,26 +2,14 @@ from django.shortcuts import render
 from django.db.models import Value, F, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.contrib.contenttypes.models import ContentType
-from django.db import transaction
+from django.db import connection, transaction
 from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-
-    # ...
-
-    # Transactions
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
+    # Executing Raw SQL Queries
+    with connection.cursor() as cursor:
+        cursor.callproc('get_customers', [1, 2, 'a'])
 
     return render(request, 'hello.html', {'name': 'Bikram'})
