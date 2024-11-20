6710da650c398c59372d6ec0a7a6e497c440fe82
bikramia <bikram.rhce@gmail.com>
2024-11-20 07:38:48 -0500

    Providing Links to tother Pages and sorting

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
-from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from . import models
 
 @admin.register(models.Product)
@@ -25,7 +26,14 @@ class CollectionAdmin(admin.ModelAdmin):
     
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist') 
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+        
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
