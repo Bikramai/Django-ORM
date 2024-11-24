59fdab6db6c719dc2c326c108d124057c9286d36
bikramia <bikram.rhce@gmail.com>
2024-11-24 10:20:28 -0500

    Creating Custom Actions

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -21,7 +21,7 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    readonly_fields = ['title']
+    actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
